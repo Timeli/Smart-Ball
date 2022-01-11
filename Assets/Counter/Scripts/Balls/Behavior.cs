5060443ab73b215056c1156c205f566cce3c987e
Timely <timbelentsov@gmail.com>
2022-01-11 23:11:13 +0300

    fc drag and drop finished

--- Assets/Counter/Scripts/Balls/Behavior.cs
+++ Assets/Counter/Scripts/Balls/Behavior.cs
@@ -2,88 +2,91 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using static UnityEngine.Random;
 
 public class Behavior : MonoBehaviour
 {
-    [SerializeField] private BallController _ballController;
+    //[SerializeField] private BallController _ballController;
 
-    private Rigidbody _body;
-    private bool _isGround;
+    //private Rigidbody _body;
+    //private bool _isGround;
 
-    private Vector3[] _directions = 
-        { 
-          Vector3.right, Vector3.left, 
-          Vector3.forward, Vector3.back 
-                                        };
+    //private Vector3[] _directions = 
+    //    { 
+    //      Vector3.right, Vector3.left, 
+    //      Vector3.forward, Vector3.back 
+    //                                    };
     
-    private void Start()
-    {
-        _body = GetComponent<Rigidbody>();
+    //private void Start()
+    //{
+    //    _body = GetComponent<Rigidbody>();
         
-    }
+    //}
 
-    private void FixedUpdate()
-    {
-        GroundChecker();
-        if (_isGround)
-            RoamAround();
+    //private void FixedUpdate()
+    //{
+    //    GroundChecker();
+    //    if (_isGround)
+    //        RoamAround();
 
+    //}
 
+    //private void RoamAround()
+    //{
+    //    if (_body.velocity.magnitude <= 0.2f)
+    //        _body.AddForce(_directions[Range(0, 4)] * Range(0.5f, 3f), ForceMode.Impulse);
+    //}
+
+    //private void ChangeBehavior(float allTimeInBox)
+    //{
        
-    }
-
-    private void RoamAround()
-    {
-        if (_body.velocity.magnitude <= 0.2f)
-            _body.AddForce(_directions[Range(0, 4)] * Range(0.5f, 3f), ForceMode.Impulse);
-    }
-
-    private void ChangeBehavior(float allTimeInBox)
-    {
-       
-    }
+    //}
 
     private void OnMouseDrag()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit raycast))
-            transform.position = raycast.point;
-    }
-
-
-    private void GroundChecker()
-    {
-        RaycastHit hit;
-        Vector3 position = new Vector3(transform.position.x, transform.position.y - 0.45f, transform.position.z);
-        if (Physics.Raycast(position, Vector3.down, out hit, 0.1f))
+        RaycastHit hitInfo;
+        bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), 
+            out hitInfo, Mathf.Infinity, 1 << LayerMask.NameToLayer("Ground")); // тут Layer в инспекторе
+        
+        if (hit)
         {
-            if (hit.collider.name.Equals("Ground"))
-            {
-                _isGround = true;
-
-            }
-        }
-        else
-        {
-            _isGround = false;
-            _body.constraints = RigidbodyConstraints.FreezeRotationX;
-            _body.constraints = RigidbodyConstraints.FreezeRotationY;
+            transform.position = hitInfo.point + Vector3.up * 3f;
         }
     }
 
 
+    //private void GroundChecker()
+    //{
+    //    RaycastHit hit;
+    //    Vector3 position = new Vector3(transform.position.x, transform.position.y - 0.45f, transform.position.z);
+    //    if (Physics.Raycast(position, Vector3.down, out hit, 0.1f))
+    //    {
+    //        if (hit.collider.name.Equals("Ground"))
+    //        {
+    //            _isGround = true;
 
-    private void OnEnable()
-    {
-        _ballController.Ticked += ChangeBehavior;
-    }
+    //        }
+    //    }
+    //    else
+    //    {
+    //        _isGround = false;
+            
+    //    }
+    //}
 
 
-    private void OnDisable()
-    {
-        _ballController.Ticked -= ChangeBehavior;
-    }
 
-    
+    //private void OnEnable()
+    //{
+    //    _ballController.Ticked += ChangeBehavior;
+    //}
+
+
+    //private void OnDisable()
+    //{
+    //    _ballController.Ticked -= ChangeBehavior;
+    //}
+
+   
 }
